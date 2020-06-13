a5c18f42cbe766176b861008c13a8faf7ac94d4c
harrisonku <harrison.h.y.ku@gmail.com>
2020-06-13 23:51:53 +0800

    v1

--- ABR.py
+++ ABR.py
@@ -2,6 +2,7 @@
 
 #NN_MODEL = "./submit/results/nn_model_ep_18200.ckpt" # model path settings
 TARGET_BUFFER = [0.5 , 1.0]
+BIT_RATE = [500.0,850.0,1200.0,1850.0]
 class Algorithm:
     def __init__(self):
     # fill your self params
@@ -17,44 +18,19 @@ class Algorithm:
 
     #Define your al
     def run(self, time, S_time_interval, S_send_data_size, S_chunk_len, S_rebuf, S_buffer_size, S_play_time_len,S_end_delay, S_decision_flag, S_buffer_flag,S_cdn_flag,S_skip_time, end_of_video, cdn_newest_id,download_id,cdn_has_frame,IntialVars):
-
-        # If you choose the marchine learning
-        '''state = []
-
-        state[0] = ...
-        state[1] = ...
-        state[2] = ...
-        state[3] = ...
-        state[4] = ...
-
-        decision = actor.predict(state).argmax()
-        bit_rate, target_buffer = decison//4, decison % 4 .....
-        return bit_rate, target_buffer'''
-
         # If you choose BBA
-        RESEVOIR = 0.5
-        CUSHION =  1.5
-        
-        if S_buffer_size[-1] < RESEVOIR:
-            bit_rate = 0    
-        elif S_buffer_size[-1] >= RESEVOIR + CUSHION and S_buffer_size[-1] < CUSHION +CUSHION:
-            bit_rate = 2
-        elif S_buffer_size[-1] >= CUSHION + CUSHION:
-            bit_rate = 3
+        if(S_time_interval[-1] != 0):
+            throughput = S_send_data_size[-1] / S_time_interval[-1]
+            for i in range(3, -1, -1):
+                if(S_buffer_size[-1] > BIT_RATE[i] / throughput):
+                    bit_rate = i
         else:
-            bit_rate = 1
+            bit_rate = 0
 
         target_buffer = 0
-        latency_limit = 4
-
-
+        latency_limit = 5
 
         return bit_rate, target_buffer, latency_limit
-
-        # If you choose other
-        #......
-
-
 
     def get_params(self):
     # get your params
